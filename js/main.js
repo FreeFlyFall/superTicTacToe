7b2cc1b5f6787f7823a0408cbe0922b0356624db
Ryan VanSlyke <ryanjvanslyke@gmail.com>
2019-02-10 23:45:12 -0500

    playable

--- js/main.js
+++ js/main.js
@@ -1,57 +1,123 @@
 const player1 = 'X';
 const player2 = 'O';
 
-const Combos = [
+const COMBOS = [
     // Rows
-    [1,2,3,4],
-    [5,6,7,8],
-    [9,10,11,12],
-    [13,14,15,16],
+    [0,1,2,3],
+    [4,5,6,7],
+    [8,9,10,11],
+    [12,13,14,15],
     // Cols
+    [0,4,8,12],
     [1,5,9,13],
     [2,6,10,14],
     [3,7,11,15],
-    [4,8,12,16],
     // Diagonals
-    [1,6,11,16],
-    [4,7,10,13],
+    [0,5,10,15],
+    [3,6,9,12],
     // Corners
-    [1,4,13,16],
+    [0,3,12,15],
     // Squares
+    [0,1,4,5],
     [1,2,5,6],
     [2,3,6,7],
-    [3,4,7,8],
+    [4,5,8,9],
     [5,6,9,10],
     [6,7,10,11],
-    [7,8,11,12],
+    [8,9,12,13],
     [9,10,13,14],
-    [10,11,14,15],
-    [11,12,15,16]
+    [10,11,14,15]
 ]
 
-const cells = $(".cell");
+const CELLS = $(".cell");
 var boardArray;
-startGame();
+var limit = 0;
+var isPlayer1;
+
 $("button").click(startGame);
+startGame();
 
 function startGame() {
+    isPlayer1 = true;
     $(".endgame").hide();
-    boardArray = [];
-    for (i = 0; i <= cells.length; i++){
-        boardArray.push(i);
-        $(cells[i]).text('');
-        $(cells[i]).css("background-color", "");
-        $(cells[i]).click(turnClick(cells[i]));
+    boardArray = Array.from(Array(16).keys());
+    for (i = 0; i <= CELLS.length; i++){
+        $(CELLS[i]).text('');
+        $(CELLS[i]).css("background-color", "");
+        if(limit == 0) {
+            $(CELLS[i]).click({param1: event}, turnClick);
+        }
     };
-    boardArray.shift();
+    limit++;
 }
 
 function turnClick(square){
-    turn(square.target.id, player1);
-}
-function turn(squareId, player){
-    boardArray[squareId - 1] = player;
-    $(`#${squareId}`).text(player);
+    if (typeof boardArray[square.target.id] == 'number'){
+        if (isPlayer1 == true){
+            turn(square.target.id, player1);
+        } else {
+            turn(square.target.id, player2);
+        }
+        isPlayer1 = !isPlayer1;
+        checkTie();
+    }
     console.log(boardArray);
 }
 
+function turn(squareId, player){
+    boardArray[squareId] = player;
+    $(`#${squareId}`).text(player);
+    let gameWon = checkWin(boardArray, player);
+    if(gameWon){
+        gameOver(gameWon);
+        console.log(gameWon);
+    }
+}
+
+function checkWin(board, player){
+    let plays = board.reduce((a, e, i) =>
+        (e === player) ? a.concat(i) : a, []);
+    let gameWon = null;
+    for (let [index, win] of COMBOS.entries()){
+        if (win.every(elem => plays.indexOf(elem) > -1)){
+            gameWon = {index: index, player: player};
+            break;
+        }
+    }
+    return gameWon;
+}
+
+function gameOver(gameWon){
+    for (let index of COMBOS[gameWon.index]) {
+        color = gameWon.player == player1 ? "blue" : "red";
+        $(`#${index}`).css("background-color", color)
+    }
+    for (var i = 0; i < CELLS.length; i++){
+        $(CELLS[i]).off();
+        limit = 0;
+    }
+    declareWinner(gameWon.player == player1 ? "Player 1 wins." : "Player 2 wins.");
+}
+
+function declareWinner(player){
+    $(".endgame").show();
+    $(".endgame").text(`${player}`);
+    console.log(`${player}`)
+}
+
+function emptySquares(){
+    return boardArray.filter(s => typeof s == 'number');
+}
+
+function checkTie() {
+    if (emptySquares().length == 0){
+        for (var i = 0; i < CELLS.length; i++){
+            $(CELLS[i]).css("background-color", "green");
+            $(CELLS[i]).off();
+            limit = 0;
+        }
+        declareWinner("Tie Game!");
+        return true;
+    }
+    return false;
+}
